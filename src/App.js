829c796dce948856a9d38f0a83d1efc94ab34526
Moritz Bergemann <49220792+Moritz-Bergemann@users.noreply.github.com>
2022-07-06 15:22:32 +0200

    fix: gradient on varying resolutions

--- src/App.js
+++ src/App.js
@@ -9,43 +9,45 @@ import "./styles.css";
 
 function App() {
 	return (
-		<div className="App">
-			<Preloader />
-			<div className="container">
-				<img
-					src={title}
-					width="900px"
-					alt="..."
-					style={{
-						marginBottom: "-30px",
-						maxWidth: "100%"
-					}}
-				/>
-			</div>
-			<div className="container">
-				<img
-					src={catchline}
-					width="600px"
-					alt="..."
-					style={{
-						marginBottom: "20px",
-						maxWidth: "70%"
-					}}
-				/>
-				<h2>
-					<span className="hackathon">
-						<strong>STARTING SOON</strong>
-					</span>
-					<br />
-				</h2>
-				<Timer />
-				<Optin />
-				<JoinDiscord />
-				<h2>
-					<span className="bottomtext">
-						<strong>Contact us</strong> at facebook.com/ComSSACurtin for further questions
-					</span>
-				</h2>
+		<div className="gradient-container">
+			<div className="App">
+				<Preloader />
+				<div className="container">
+					<img
+						src={title}
+						width="900px"
+						alt="..."
+						style={{
+							marginBottom: "-30px",
+							maxWidth: "100%"
+						}}
+					/>
+				</div>
+				<div className="container">
+					<img
+						src={catchline}
+						width="600px"
+						alt="..."
+						style={{
+							marginBottom: "20px",
+							maxWidth: "70%"
+						}}
+					/>
+					<h2>
+						<span className="hackathon">
+							<strong>STARTING SOON</strong>
+						</span>
+						<br />
+					</h2>
+					<Timer />
+					<Optin />
+					<JoinDiscord />
+					<h2>
+						<span className="bottomtext">
+							<strong>Contact us</strong> at facebook.com/ComSSACurtin for further questions
+						</span>
+					</h2>
+				</div>
 			</div>
 		</div>
 	);
